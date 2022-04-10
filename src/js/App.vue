6b1185c9fde25cb706296fd4398fda4bf6e0723b
Anna Gallo <61474460+galloanna@users.noreply.github.com>
2022-04-10 15:06:47 -0400

    Dev/utility setup (#1)

---
+++ src/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>Hello, Vue!</div>
+    <Counter />
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter.vue';
+export default {
+  name: 'App',
+  components: {
+    Counter,
+  },
+};
+</script>
---
+++ src/js/components/Counter.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="counter">
+    <h1>{{ count }}</h1>
+    <button @click="increase">+</button>
+    <button @click="decrease">1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  data: () => ({
+    count: 0,
+  }),
+  methods: {
+    increase() {
+      this.count++;
+    },
+    decrease() {
+      this.count--;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.counter {
+  button {
+    background-color: purple;
+    padding: 20px;
+  }
+  background-color: red;
+  border-radius: 10px;
+  color: white;
+  display: inline-block;
+  padding: 10px 15px 20px 10px;
+  text-align: center;
+  width: 100px;
+}
+</style>
