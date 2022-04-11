5c82b7ba9e5d7dedee58ad0a5a21ac3f201180aa
galloanna <heyannagallo@gmail.com>
2022-04-10 20:35:28 -0400

    start search and filtering

--- src/js/App.vue
+++ src/js/App.vue
@@ -1,16 +1,48 @@
 <template>
   <div>
-    <div>Hello, Vue!</div>
-    <Counter />
+    <Filters :filterItems="filterItems" :search="search" :filteredItems="filteredItems" />
+    <Items :items="items" />
   </div>
 </template>
 
 <script>
-import Counter from './components/Counter.vue';
+import Filters from './components/Filters.vue';
+import Items from './components/Items.vue';
+
+import dataSet from './data/data.json';
+const mediaData = dataSet.media;
+
 export default {
   name: 'App',
   components: {
-    Counter,
+    Filters,
+    Items,
+  },
+  data() {
+    return {
+      items: mediaData,
+      str: '',
+      type: '',
+    };
+  },
+  methods: {
+    filterItems(mediaType) {
+      this.resetItems();
+      if (mediaType !== 'All') {
+        this.items = this.items.filter((item) => {
+          return item.type === mediaType;
+        });
+      }
+    },
+    search(term) {
+      this.resetItems();
+      this.items = this.items.filter((item) => {
+        return item.title.toLowerCase().includes(term.toLowerCase());
+      });
+    },
+    resetItems() {
+      this.items = mediaData;
+    },
   },
 };
 </script>
