fd57bb87ca67b56a1e2a15b5ba17f7815d9a8ba3
Anna Gallo <61474460+galloanna@users.noreply.github.com>
2022-04-12 00:07:30 -0400

    start search and filtering (#3)

---
+++ src/js/components/Filters.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2 class="sr-only">Filters</h2>
+    <label for="search">
+      Search
+      <input id="search" v-model="term" @keypress.enter="search(term)" />
+    </label>
+
+    <p v-for="filter in filters" :key="filter" @click="() => filterItems(filter)">
+      {{ filter }}
+    </p>
+  </div>
+</template>
+
+<script>
+const filters = ['All', 'book', 'movie'];
+
+export default {
+  props: ['filterItems', 'search', 'filteredItems'],
+  data() {
+    return {
+      filters,
+      term: '',
+    };
+  },
+};
+</script>
---
+++ src/js/components/Items.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1 class="sr-only">Available Media</h1>
+    <div class="media-items">
+      <div class="media-item" v-for="item in items" :key="item.id">
+        <img class="media-item__poster" :src="item.poster" />
+        <h3 class="media-item__title">{{ item.title }} ({{ item.year }})</h3>
+        <h4 class="media-item__genres">Genres: {{ item.genre }}</h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['items'],
+};
+</script>
+
+<style lang="scss" scoped>
+.media-items {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 10px;
+  }
+.media-item {
+  width: 30%;
+}
+
+.media-item__poster {
+  width: 100%;
+}
+</style>
