c64ef708da5a4395d4385ed510cf895dd3dfaba0
galloanna <heyannagallo@gmail.com>
2023-08-14 00:23:25 -0400

    fix: change style lang

--- src/js/components/ClearButton.vue
+++ src/js/components/ClearButton.vue
@@ -13,18 +13,19 @@ export default {
 };
 </script>
 
-<style lang="sass" scoped>
-@import '../../styles/index.scss'
-.filters__clear-button
-    align-self: flex-end
-    -webkit-appearance: none
-    -moz-appearance: none
-    background-color: transparent
-    text-decoration: underline
-    color: $blue
-    font-family: 'Montserrat'
-    text-transform: uppercase
-    font-weight: 700
-    border: 0
-    cursor: pointer
+<style lang="scss" scoped>
+@import '../../styles/index.scss';
+.filters__clear-button {
+    align-self: flex-end;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    background-color: transparent;
+    text-decoration: underline;
+    color: $blue;
+    font-family: 'Montserrat';
+    text-transform: uppercase;
+    font-weight: 700;
+    border: 0;
+    cursor: pointer;
+}
 </style>
