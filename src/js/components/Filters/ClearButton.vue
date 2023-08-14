fa1e0800c3e8e4172bf9cad8574e4c3355c61b96
galloanna <heyannagallo@gmail.com>
2023-08-14 00:27:30 -0400

    refactor: update clear path and imports

---
+++ src/js/components/Filters/ClearButton.vue
@@ -0,0 +1,31 @@
+<template>
+    <button class="filters__clear-button" @click="resetFilters">Clear Filters</button>
+</template>
+
+<script>
+export default {
+    props: {
+        resetFilters: {
+            type: Function,
+            required: true
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../../styles/index.scss';
+.filters__clear-button {
+    align-self: flex-end;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    background-color: transparent;
+    text-decoration: underline;
+    color: $blue;
+    font-family: 'Montserrat';
+    text-transform: uppercase;
+    font-weight: 700;
+    border: 0;
+    cursor: pointer;
+}
+</style>
