a4351b09a15af6141c21ff47f8703e015c1ef277
Anna Gallo <61474460+galloanna@users.noreply.github.com>
2023-08-15 20:21:56 -0400

    fix: a11y features (#14)

--- src/js/components/Filters/TypesFilter.vue
+++ src/js/components/Filters/TypesFilter.vue
@@ -1,10 +1,14 @@
 <template>
     <div class="filters__types">
-        <legend class="sr-only">Choose a media type</legend>
-        <label tabindex="0" class="filters__type-label" :for="type" v-for="(type, index) in types" :key="type">
-            <input tabindex="0" class="filters__type-input" type="radio" :id="type" :value="type" :checked="selectedTypes.includes(type)" @change="updateSelectedType(type)" />
-            {{ type.charAt(0).toUpperCase() + type.substr(1) + 's' }}
-        </label>
+        <form>
+            <fieldset>
+                <legend class="sr-only">Choose a media type</legend>
+                <label tabindex="0" class="filters__type-label" :for="type" v-for="(type, index) in types" :key="type">
+                    <input tabindex="0" class="filters__type-input" type="radio" :id="type" :value="type" :checked="selectedTypes.includes(type)" @change="updateSelectedType(type)" />
+                    {{ type.charAt(0).toUpperCase() + type.substr(1) + 's' }}
+                </label>
+            </fieldset>
+        </form>
     </div>
 </template>
 
@@ -28,18 +32,23 @@ export default {
     font-family: 'Montserrat';
     font-size: 14px;
 }
-.filters__type {
+fieldset {
+    border: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+}
+.filters__type-label {
+    font-weight: 700;
     display: flex;
     cursor: pointer;
-    &-label {
-        font-weight: 700;
-        &:not(:last-child) {
-            margin-right: 10px;
-        }
-    }
-    &-input {
-        margin: 0 5px 0 0;
-        padding: 0;
-    }
+}
+.filters__type-label:not(:last-child) {
+    margin-right: 10px;
+}
+.filters__type-input {
+    margin: 0 5px 0 0;
+    padding: 0;
+    cursor: pointer;
 }
 </style>
